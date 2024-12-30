c2490af0ff923d8f77d07af082e4139c1100defc
AllanNguyen <allandog06@gmail.com>
2024-12-30 10:20:21 -0800

    Added an env file to store the db password

--- api/repositories/database.js
+++ api/repositories/database.js
@@ -1,10 +1,12 @@
 const mysql = require("mysql2");
+require("dotenv").config();
 
 const connection = mysql.createConnection({
     host: "bdfstnwx1ihai75szfde-mysql.services.clever-cloud.com",
     user: "udicjo53zlxs9oqa",
-    password: "PVi5PxcIynod0OYJoq41",
+    password: process.env.DB_PASSWORD,
     database: "bdfstnwx1ihai75szfde",
+    port: 3306
 });
 
 module.exports = connection;
