b42f09319c20ab2f1dbfb3befba78c6b286a14c8
AllanNguyen <allandog06@gmail.com>
2024-12-30 22:12:29 -0800

    Went to freemysqlhosting for the db because both aiven and clever cloud won't work

--- api/repositories/database.js
+++ api/repositories/database.js
@@ -1,18 +1,12 @@
 const mysql = require("mysql2");
-const fs = require("fs");
-const ca = fs.readFileSync("./ca.pem");
 
 require("dotenv").config();
-console.log(ca.toString());
+
 const connection = mysql.createConnection({
-    host: "mysql-2e67ef58-chattington.l.aivencloud.com",
-    user: "avnadmin",
+    host: "sql3.freemysqlhosting.net",
+    user: "sql3754972",
     password: process.env.DB_PASSWORD,
-    database: "defaultdb",
-    port: 15569,
-    ssl: {
-        ca: ca
-    }
+    database: "sql3754972"
 });
 
 module.exports = connection;
