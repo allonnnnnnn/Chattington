c64cc4fb6803a4e3cd37c098ce191ea3315f76a6
AllanNguyen <allandog06@gmail.com>
2024-12-31 19:42:25 -0800

    Changed web socket port

--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@ const port = 8000;
 const secret = crypto.randomBytes(64).toString("hex");
 const clients = {};
 
-const socketServer = new webSocket.Server({ port: 3000 });
+const socketServer = new webSocket.Server({ port: 321 });
 
 app.use("/images", express.static("./public/images"));
 app.use("/css", express.static("./public/css"));
